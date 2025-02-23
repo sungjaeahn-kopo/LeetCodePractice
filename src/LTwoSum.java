b91b6579377568b06f5857bf4d724e592b9ffc53
sungjae_ahn <sungjae_ahn2@tmax.co.kr>
2025-02-23 17:47:21 +0900

    initial commit

---
+++ src/LTwoSum.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class LTwoSum {
+    public static void main(String[] args) {
+        int[] result = LTwoSum.twoSum(new int[] {2, 7, 11, 15}, 9);
+
+        for (int value : result) {
+            System.out.println(value);
+        }
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target == nums[i] + nums[j]) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+
+        return result;
+    }
+}
