f19b199265b70fc10d7176508ccbf43a49bbaae6
sungjae_ahn <sungjae_ahn2@tmax.co.kr>
2025-02-25 15:12:10 +0900

    [TwoPointer] removeDuplicates

--- src/LArray26.java
+++ src/LArray26.java
@@ -5,33 +5,26 @@
 
 public class LArray26 {
     public static void main(String[] args) {
-        int result = LArray26.removeDuplicates(new int[] {1,1,2});
+        int result = LArray26.removeDuplicates(new int[] {0,0,1,1,1,2,2,3,3,4});
 
         System.out.println(result);
     }
 
     public static int removeDuplicates(int[] nums) {
-        int uniqueCount = 0;
-        int pointerOne = 0;
-        int pointerTwo = pointerOne + 1;
+        if (nums.length == 0) return 0; // 예외 처리
 
-        while(pointerOne < pointerTwo) {
-            if (nums[pointerOne] == nums[pointerTwo]) {
-                pointerTwo++;
-            } else {
-                uniqueCount++;
-                nums[pointerOne + 1] = nums[pointerTwo];
-                if (pointerTwo != nums.length - 1) {
-                    pointerOne++;
-                    pointerTwo++;
-                }
+        int pointerOne = 0;  // 중복 없는 요소 저장 위치
+        int pointerTwo = 1;  // 탐색 포인터
+
+        while (pointerTwo < nums.length) {
+            if (nums[pointerOne] != nums[pointerTwo]) {
+                pointerOne++;
+                nums[pointerOne] = nums[pointerTwo]; // 중복 없는 값 저장
             }
+            pointerTwo++;  // 탐색 포인터 이동
         }
 
-        // for (int e : nums) {
-        //     System.out.println(e);
-        // }
-
-        return uniqueCount;
+        return pointerOne + 1; // 유니크한 개수 반환
     }
+
 }
